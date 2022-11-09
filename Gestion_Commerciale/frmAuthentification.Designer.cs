c748e99f16af9d02f7913285b48ae703cfd77986
qrulleau <quentin.rulleau@yahoo.com>
2022-11-09 11:33:56 +0100

    ADD: config file

---
+++ Gestion_Commerciale/frmAuthentification.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Gestion_Commerciale
+{
+    partial class frmAuthentification
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtMDP = new System.Windows.Forms.TextBox();
+            this.lblMDP = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.Location = new System.Drawing.Point(590, 366);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(75, 23);
+            this.btnConnexion.TabIndex = 0;
+            this.btnConnexion.Text = "Connexion";
+            this.btnConnexion.UseVisualStyleBackColor = true;
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(453, 366);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitter.TabIndex = 1;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.AutoSize = true;
+            this.btnLogin.Location = new System.Drawing.Point(450, 187);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(84, 13);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Nom d\'utilisateur";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(565, 187);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 3;
+            // 
+            // txtMDP
+            // 
+            this.txtMDP.Location = new System.Drawing.Point(565, 236);
+            this.txtMDP.Name = "txtMDP";
+            this.txtMDP.Size = new System.Drawing.Size(100, 20);
+            this.txtMDP.TabIndex = 4;
+            // 
+            // lblMDP
+            // 
+            this.lblMDP.AutoSize = true;
+            this.lblMDP.Location = new System.Drawing.Point(450, 243);
+            this.lblMDP.Name = "lblMDP";
+            this.lblMDP.Size = new System.Drawing.Size(74, 13);
+            this.lblMDP.TabIndex = 5;
+            this.lblMDP.Text = "Mot De Passe";
+            // 
+            // frmAuthentification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 716);
+            this.Controls.Add(this.lblMDP);
+            this.Controls.Add(this.txtMDP);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnConnexion);
+            this.Name = "frmAuthentification";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnexion;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Label btnLogin;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtMDP;
+        private System.Windows.Forms.Label lblMDP;
+    }
+}
+
---
+++ Gestion_Commerciale/frmAuthentification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_Commerciale
+{
+    public partial class frmAuthentification : Form
+    {
+        public frmAuthentification()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
