21b854453bc2fd49d6dffb31d7a54045ea316ab7
ethxn-frs <114412840+ethxn-frs@users.noreply.github.com>
2022-11-12 13:38:44 +0100

    Correction frm Produits

--- Gestion_Commerciale/frmProduits.cs
+++ Gestion_Commerciale/frmProduits.cs
@@ -17,16 +17,6 @@ namespace Gestion_Commerciale
             InitializeComponent();
         }
 
-        private void lblTitre_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
         private void btnRetour_Click(object sender, EventArgs e)
         {
             this.Close(); 
